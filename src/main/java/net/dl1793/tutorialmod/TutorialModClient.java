d27c56c03a823274cef03214e0de6edc5978f607
DL1793 <mihainiculescuc@gmail.com>
2025-06-02 20:25:49 +0300

    Initial commit

---
+++ src/main/java/net/dl1793/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.dl1793.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
